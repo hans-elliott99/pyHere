077cf794ce61dc46164eaa28d9f6d86920d6ed61
Hans Elliott <hanselliott61@gmail.com>
2023-05-14 16:17:09 -0700

    added variable length arg list for methods. added exists method for convenience.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # pyHere
-A super simple [r-lib/here](https://github.com/r-lib/here) for python.  
-For now, available on test-pypi: https://test.pypi.org/project/pyHere/0.0.5/  
-
+A super simple [r-lib/here](https://github.com/r-lib/here) for python.    
+For now, available on test-pypi: https://test.pypi.org/project/pyHere/0.0.6/  
--- src/pyHere/pyhere.py
+++ src/pyHere/pyhere.py
@@ -3,8 +3,8 @@ from pathlib import Path
 class Here:
     """Simplify filepath management throughout your project.
 
-    To use pyHere create an instance of Here and specify the name of the root 
-    directory so that Here knows where your project starts.  
+    Create an instance of Here and specify the name of the root directory so 
+    that Here knows where your project starts.  
     Use Here.here("path/from/inside/root") to get the absolute path to a file or 
     folder inside your root directory.  
     pyHere.Here is built ontop of python's pathlib module, and can be set to return 
@@ -16,13 +16,14 @@ class Here:
         If True, pathlib.Path instances will be returned instead of strings.
     :type as_string: bool
 
+
     Example usage:
 
     .. code-block:: python
         >>> here = Here(project_dir = "my_project")
         >>> here.here("folder_1/dataset1.csv")
-        "C:/users/ExampleUser/Documents/my_project/folder_2/dataset2.csv"
-        >>> here.here("folder_2/dataset2.csv", as_path=True)
+        "C:/users/ExampleUser/Documents/my_project/folder_1/dataset1.csv"
+        >>> here.here("folder_1/dataset1.csv", as_path=True)
         WindowsPath("C:/users/ExampleUser/Documents/my_project/folder_1/dataset1.csv")
     """
 
@@ -30,15 +31,22 @@ class Here:
         top = str(project_dir).replace("\\", "/").replace("/", "").lstrip(".")            
         cwd = Path().cwd().as_posix().split("/")
         assert top in cwd, \
-            "Top directory not found in working path. Ensure that 'top_directory' is an existing directory."
+            "Top directory not found in working path. Ensure that 'project_dir' is an existing directory."
         self.root = Path('/'.join(cwd[0 : cwd.index(top) + 1]))
         self._as_pthlib = as_path
 
-    def here(self, relative_path="", as_path=None):
+    def _abs_path(self, *rel_pth):
+        # Get absolute path to provided relative path w/in project dir
+        rel_pth = '/'.join([str(a).strip("/\\") for a in rel_pth])
+        return self.root / Path(rel_pth)
+
+    def here(self, *relative_path, as_path=None):
         """Get the absolute path to a file or folder within your project directory.
 
-        :param relative_path: The relative path from the root/project directory to the target 
-            file or folder. If not provided, returns the absolute path to the root directory.
+        :param *relative_path: The relative path from the root/project directory to the target 
+            file or folder. Can be concatenated as one string, or each folder/file can
+            be listed separately (variable length arg list).
+            If not provided, the absolute path to the root directory will be returned.
         :param type: Union[str, pathlib.Path]
         :param as_path: Default None. If True, return the path as a pathlib.Path instance. If False, 
             return as a string. If None, use the default value set at initialization, which itself  
@@ -47,13 +55,28 @@ class Here:
         :return: The absolute path to the provide relative path.
         :rtype: Union[str, pathlib.Path]
         """
-        p = self.root / Path(relative_path)
+        p = self._abs_path(*relative_path)
         if as_path is None:
             as_path = self._as_pthlib
         if as_path:
             return p
         else:
             return p.as_posix()
-    
+
+    def exists(self, *relative_path):
+        """Check if a file exists within your project directory.
+        Simply wraps pathlib.exists, so a user doesn't have to do:
+            Here.here("relative/path", as_path=True).exists()
+
+        :param *relative_path: The relative path from the root/project directory to the target 
+            file or folder. Can be concatenated as one string, or each folder/file can
+            be listed separately (variable length arg list).
+            If not provided, the absolute path to the root directory will be checked.
+        :param type: Union[str, pathlib.Path]
+        """
+        p = self._abs_path(*relative_path)
+        return p.exists()
+
+
     def __str__(self):
         return self.root.as_posix()
